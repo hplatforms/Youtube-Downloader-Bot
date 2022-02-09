0a2cd627b5185a57f0011b4ae4cdbe5b017ba3ef
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-09 13:22:20 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"**Merhaba**\n\nBen Bir YouTube Video ve Ses İndirme Botuyum. Beni Kullanmak İçin Bana Bir YouTube URL Gönder VEYA @vid Yazarak İstediğin Videoyu Arayarak Bulabilirisin.\n\n**Geliştirici**: @trbotlistesi\n**Yardım**: @trbotlistesidestek"
+    helptxt = f"**Merhaba**\n\nBen Bir YouTube Video ve Ses İndirme Botuyum. Beni Kullanmak İçin Bana Bir YouTube URL Gönder VEYA @vid Yazarak İstediğin Videoyu Arayarak Bulabilirisin. Unutma Playlist Linklerini Desteklemiyorum.\n\n**Geliştirici**: @trbotlistesi\n**Yardım**: @trbotlistesidestek"
     await message.reply_text(helptxt)
