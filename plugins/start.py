13207828414101a467daf49b661d5a53f48f5774
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-10 16:38:43 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,11 +4,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Kanal", url="https://t.me/trbotlistesi")],
-        [InlineKeyboardButton("Bildir", url="https://t.me/trbotlistesidestek")],
-        [InlineKeyboardButton("iletişim", url="https://t.me/trbotlistesidestek")]
-    ])
+    joinButton = InlineKeyboardMarkup[[
+        InlineKeyboardButton("Kanal", url="https://t.me/trbotlistesi"),
+        InlineKeyboardButton("Bildir", url="https://t.me/trbotlistesidestek")
+    ],[
+        InlineKeyboardButton("iletişim", url="https://t.me/trbotlistesidestek"),
+        InlineKeyboardButton("iletişim", url="https://t.me/trbotlistesidestek")
+    ]]
     welcomed = f"Merhaba <b>{message.from_user.first_name}</b>\nBen Bir YouTube Video ve Ses İndirme Botuyum. Beni Kullanmak İçin Bana Bir YouTube URL Gönder VEYA @vid Yazarak İstediğin Videoyu Arayarak Bulabilirisin"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
