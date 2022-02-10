15103ab1acd79a7b3ee6622e1f8872d4ce577d1e
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-10 16:23:06 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,8 +6,8 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Kanal", url="https://t.me/trbotlistesi")],
-        [InlineKeyboardButton(
-            "Bildir", url="https://t.me/trbotlistesidestek")]
+        [InlineKeyboardButton("Bildir", url="https://t.me/trbotlistesidestek")],
+        [InlineKeyboardButton("iletişim", url="https://t.me/trbotlistesidestek")]
     ])
     welcomed = f"Merhaba <b>{message.from_user.first_name}</b>\nBen Bir YouTube Video ve Ses İndirme Botuyum. Beni Kullanmak İçin Bana Bir YouTube URL Gönder VEYA @vid Yazarak İstediğin Videoyu Arayarak Bulabilirisin"
     await message.reply_text(welcomed, reply_markup=joinButton)
